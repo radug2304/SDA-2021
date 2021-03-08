19a0e08563bd2827737e875fe11804b9f7a79610
Iovanovici Rada-George <rada.iovanovici@student.unitbv.ro>
2021-03-08 21:40:31 +0200

    Tema Lab1

--- src/Lab1/Exercitiul1.java
+++ src/Lab1/Exercitiul1.java
@@ -4,7 +4,15 @@ public class Exercitiul1 {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
-
+		Operations op = new Operations();
+		
+		op.read();
+		op.sum();
+		op.difference();
+		op.product();
+		op.average();
+		op.distance();
+		op.comparison();
 	}
 
 }
---
+++ src/Lab1/Operations.java
@@ -0,0 +1,64 @@
+package Lab1;
+import java.util.Scanner;
+public class Operations {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	int firstNumber;
+	int secondNumber;
+	public void read() {
+		try
+		{
+		System.out.println("First number : ");
+		firstNumber = sc.nextInt();
+		System.out.println("Second number : ");
+		secondNumber = sc.nextInt();	
+		}catch(Exception e) {
+			System.out.println("Just numbers please :) , try again!");
+			sc.nextLine();
+			read();
+		}
+		
+		sc.close();
+	}
+	
+	public void sum() {
+		int sum = firstNumber+secondNumber;
+		System.out.println("Sum of numbers is : " + sum);
+	}
+	
+	public void difference() {
+		int diff = firstNumber - secondNumber;
+		System.out.println("Difference on numbers is : " + diff);
+		
+	}
+	
+	public void product() {
+		int prod = firstNumber * secondNumber;
+		System.out.println("Product of numbers is : " + prod);
+	}
+	
+	public void average() {
+		double avg = (firstNumber + secondNumber)/2;
+		System.out.println("Average is : " + avg);
+	}
+	
+	public void distance() {
+		int dist = firstNumber - secondNumber;
+		System.out.println("Distance is : " + Math.abs(dist));
+	}
+	
+	public void comparison() {
+		if(firstNumber >= secondNumber) {
+			System.out.println("Maximum number is : " + firstNumber);
+			System.out.println("Minimum number is : " + secondNumber);
+		}
+		
+		else
+			
+		{
+			System.out.println("Maximum number is : " + secondNumber);
+			System.out.println("Minimum number is : " + firstNumber);
+		}
+	}
+}
